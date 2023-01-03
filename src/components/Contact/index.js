f0231e9dc1a910057ce6fd1083445b4787d965f8
henrybenso <henrykbenso1@gmail.com>
2023-01-03 00:58:14 -0800

    completed

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,81 +1,122 @@
 import { Bars } from 'react-loader-spinner'
 import AnimatedLetters from '../AnimatedLetters'
 import './index.scss'
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useRef } from 'react'
+import emailjs from '@emailjs/browser'
+import { MapContainer, Marker, TileLayer, Popup } from 'react-leaflet'
 
 const Contact = () => {
-    const [letterClass, setLetterClass] = useState('text-animate')
+  const [letterClass, setLetterClass] = useState('text-animate')
+  const refForm = useRef()
 
-    useEffect(() => {
-        return setTimeout(() => {
-            setLetterClass('text-animate-hover')
-        }, 3000)
-    }, [])
+  useEffect(() => {
+    setTimeout(() => {
+      setLetterClass('text-animate-hover')
+    }, 3000)
+  }, [])
 
-    return (
-        <>
-            <div className='container contact-page'>
-                <div className='text-zone'>
-                    <h1>
-                        <AnimatedLetters
-                        letterClass={letterClass}
-                        strArray={['C', 'o', 'n', 't', 'a', 'c', 't', ' ', 'm', 'e']}
-                        idx={15}
-                        />
-                    </h1>
-                    <p>
-                        I am interested in asdfasdgojajasdg bananan bannbnanbanbn abnanbanbnabnanb
-                        rswtyjnzrnjy
-                        swtrymjknntkm7yyswtmky 77
-                    </p>
-                    <div className="contact-form">
-                        <form action='https://www.actionforms.io/e/r/myportfolio' method="any">
-                            <ul>
-                                <li className="half">
-                                    <input type="text" name="name" placeholder="Name" required />
-                                </li>
-                                <li className='half'>
-                                    <input
-                                        type="email"
-                                        name="email"
-                                        placeholder="Email"
-                                        required
-                                    />
-                                </li>
-                                <li>
-                                    <input
-                                        placeholder="Subject"
-                                        type="text"
-                                        name="subject"
-                                        required
-                                    />
-                                </li>
-                                <li>
-                                    <textarea
-                                    placeholder='Message'
-                                    name="message"
-                                    required
-                                    ></textarea>
-                                </li>
-                                <li>
-                                    <input type="submit" className='lat-button' value="SEND" />
-                                </li>
-                            </ul>
-                        </form>
-                    </div>
-                </div>
-            </div>
-            <Bars
-                height="80"
-                width="80"
-                color="#FFFFFF"
-                ariaLabel="bars-loading"
-                wrapperStyle={{}}
-                wrapperClass="loader-active"
-                visible={true}
+  const sendEmail = (e) => {
+    e.preventDefault()
+
+    emailjs
+      .sendForm(
+        'service_v5m3b5a',
+        'template_s5exof8',
+        refForm.current,
+        'NiPujQbKepmyzpxLU'
+      )
+      .then(
+        () => {
+          alert('Message successfully sent!')
+          window.location.reload(false)
+        },
+        () => {
+          alert('Failed to send the message, please try again.')
+        }
+      )
+  }
+
+  return (
+    <>
+      <div className="container contact-page">
+        <div className="text-zone">
+          <h1>
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={['C', 'o', 'n', 't', 'a', 'c', 't', ' ', 'm', 'e']}
+              idx={15}
             />
-        </>
-    )
+          </h1>
+          <p>
+            I am interested in asdfasdgojajasdg bananan bannbnanbanbn
+            abnanbanbnabnanb rswtyjnzrnjy swtrymjknntkm7yyswtmky 77
+          </p>
+          <div className="contact-form">
+            <form ref={refForm} onSubmit={sendEmail}>
+              <ul>
+                <li className="half">
+                  <input type="text" name="name" placeholder="Name" required />
+                </li>
+                <li className="half">
+                  <input
+                    type="email"
+                    name="email"
+                    placeholder="Email"
+                    required
+                  />
+                </li>
+                <li>
+                  <input
+                    placeholder="Subject"
+                    type="text"
+                    name="subject"
+                    required
+                  />
+                </li>
+                <li>
+                  <textarea
+                    placeholder="Message"
+                    name="message"
+                    required
+                  ></textarea>
+                </li>
+                <li>
+                  <input type="submit" className="flat-button" value="SEND" />
+                </li>
+              </ul>
+            </form>
+          </div>
+        </div>
+        <div className="info-map">
+          Henry Benso,
+          <br />
+          USA
+          <br />
+          <span>hbenso@lion.lmu.edu</span>
+        </div>
+        <div className="map-wrap">
+          <MapContainer
+            center={[33.97092738768288, -118.41703485966558]}
+            zoom={13}
+          >
+            <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+            <Marker position={[33.97092738768288, -118.41703485966558]}>
+              <Popup>You can find me here :)</Popup>
+            </Marker>
+          </MapContainer>
+        </div>
+      </div>
+      <Bars
+        height="80"
+        width="80"
+        color="#FFFFFF"
+        ariaLabel="bars-loading"
+        wrapperStyle={{}}
+        wrapperClass="loader-active"
+        visible={true}
+      />
+    </>
+  )
 }
 
 export default Contact
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -2,67 +2,125 @@ import { Link } from 'react-router-dom'
 import { useEffect, useState } from 'react'
 import AnimatedLetters from '../AnimatedLetters'
 import Logo from './Logo'
-import LogoTitle from '../../assets/images/logo-s.png';
+import LogoTitle from '../../assets/images/logo-s.png'
 import './index.scss'
-import { Bars } from 'react-loader-spinner';
+import { Bars } from 'react-loader-spinner'
 
 const Home = () => {
-    const [letterClass, setLetterClass] = useState('text-animate')
-    const nameArray = ['e', 'n', 'r', 'y']
-    const jobArray = ['a', 's', 'p', 'i', 'r', 'i', 'n', 'g', ' ', 'w', 'e', 'b', ' ', 'd', 'e', 'v', 'e', 'l', 'o', 'p', 'e', 'r']
-    const andArray = ['a', 'n', 'd']
-    const schoolArray = ['C', 'S', ' ', 's', 't', 'u', 'd', 'e', 'n', 't', ' ', 'a', 't', ' ', 'L', 'M', 'U', '.']
+  const [letterClass, setLetterClass] = useState('text-animate')
+  const nameArray = ['e', 'n', 'r', 'y']
+  const jobArray = [
+    'a',
+    's',
+    'p',
+    'i',
+    'r',
+    'i',
+    'n',
+    'g',
+    ' ',
+    'f',
+    'u',
+    'l',
+    'l',
+    '-',
+    's',
+    't',
+    'a',
+    'c',
+    'k',
+    ' ',
+    'd',
+    'e',
+    'v',
+    'e',
+    'l',
+    'o',
+    'p',
+    'e',
+    'r',
+  ]
+  const andArray = ['a', 'n', 'd']
+  const schoolArray = [
+    'C',
+    'S',
+    ' ',
+    's',
+    't',
+    'u',
+    'd',
+    'e',
+    'n',
+    't',
+    ' ',
+    'a',
+    't',
+    ' ',
+    'L',
+    'M',
+    'U',
+    '.',
+  ]
 
+  useEffect(() => {
+    setTimeout(() => {
+      setLetterClass('text-animate-hover')
+    }, 4000)
+  }, [])
 
-    useEffect(() => {
-        return setTimeout(() => {
-            setLetterClass('text-animate-hover')
-        }, 4000)
-    }, [])
-
-    return (
-        <>
-        <div className="container home-page">
-            <div className="text-zone">
-                <h1>
-                <span className={letterClass}>H</span>
-                <span className={`${letterClass} _12`}>i,</span>
-                    <br />
-                <span className={`${letterClass} _13`}>I</span>
-                <span className={`${letterClass} _14`}>'m</span>
-                <img src={LogoTitle} alt="developer" />
-                <AnimatedLetters letterClass={letterClass}
-                strArray={nameArray}
-                idx={15} />
-                <br />
-                <AnimatedLetters letterClass={letterClass}
-                strArray={jobArray}
-                idx={19} />
-                 <br />
-                <AnimatedLetters letterClass={letterClass}
-                strArray={andArray}
-                idx={28} />
-                <br />
-                <AnimatedLetters letterClass={letterClass}
-                strArray={schoolArray}
-                idx={31} />
-                </h1>
-                <h2>Fullstack Developer / Keyboard Enthusiast</h2>
-                <Link to="/contact" className='flat-button'>CONTACT ME</Link>
-            </div>
-            <Logo />
+  return (
+    <>
+      <div className="container home-page">
+        <div className="text-zone">
+          <h1>
+            <span className={letterClass}>H</span>
+            <span className={`${letterClass} _12`}>i,</span>
+            <br />
+            <span className={`${letterClass} _14`}>I</span>
+            <span className={`${letterClass} _15`}>'m</span>
+            <img src={LogoTitle} alt="developer" />
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={nameArray}
+              idx={17}
+            />
+            <br />
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={jobArray}
+              idx={21}
+            />
+            <br />
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={andArray}
+              idx={50}
+            />
+            <br />
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={schoolArray}
+              idx={53}
+            />
+          </h1>
+          <h2>Developer / Keyboard Enthusiast</h2>
+          <Link to="/contact" className="flat-button">
+            CONTACT ME
+          </Link>
         </div>
-        <Bars
-                height="80"
-                width="80"
-                color="#FFFFFF"
-                ariaLabel="bars-loading"
-                wrapperStyle={{}}
-                wrapperClass="loader-active"
-                visible={true}
-        />
-        </>
-    )
+        <Logo />
+      </div>
+      <Bars
+        height="80"
+        width="80"
+        color="#FFFFFF"
+        ariaLabel="bars-loading"
+        wrapperStyle={{}}
+        wrapperClass="loader-active"
+        visible={true}
+      />
+    </>
+  )
 }
 
 export default Home
